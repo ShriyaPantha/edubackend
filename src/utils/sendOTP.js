a9aa7542f7b2adea47de50f5edae239a34d5d31b
ShriyaPantha <shriyapantha888@gmail.com>
2026-07-01 17:36:52 +0545

    first

--- src/utils/sendOTP.js
+++ src/utils/sendOTP.js
@@ -2,15 +2,18 @@ const nodemailer = require("nodemailer");
 
 const sendOTP = async (email, otp) => {
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // true for port 465
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-    family: 4, // force IPv4 — fixes ENETUNREACH on Render
-  });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  family: 4,
+  connectionTimeout: 10000, // 10s
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+});
 
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
