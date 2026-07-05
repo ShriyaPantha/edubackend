1268c62cc61ccd9e95ab3d04efbc6bb1728705e2
ShriyaPantha <shriyapantha888@gmail.com>
2026-07-05 14:05:20 +0545

    Remove node_modules from tracking

--- src/utils/sendOTP.js
+++ src/utils/sendOTP.js
@@ -4,7 +4,7 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: true, // true for port 465
+  secure: false, // true for port 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
